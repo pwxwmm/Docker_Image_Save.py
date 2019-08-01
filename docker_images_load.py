463bc4fd3855de7553b61e92db9583e0d7284c50
pwxwmm <45311104+pwxwmm@users.noreply.github.com>
2019-08-01 20:58:43 +0800

    Create docker_images_load.py

---
+++ docker_images_load.py
@@ -0,0 +1,17 @@
+#导入镜像
+#python dockerload.py Images.tar.gz
+import os
+import sys
+if __name__ == "__main__":
+    tarball = sys.argv[1]
+    print(tarball)
+    workdir = '/tmp/xfleet-images'
+    os.system('rm -rf %s'%workdir)
+    os.system('mkdir -p %s'%workdir)
+    os.system('tar -zxvf %s -C %s'%(tarball, workdir))
+
+    os.chdir(workdir)
+    files = os.listdir(workdir)
+    for filename in files:
+        print(filename)
+        os.system('docker load -i %s'%filename)
